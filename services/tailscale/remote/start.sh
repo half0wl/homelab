2d096f0136a7b0a5e34efe2e7363a442eb962b88
Ray Chen <ray@raychen.io>
2024-01-07 19:36:55 +0800

    service(tailscale-remote): Final Caddy shenanigans?

--- services/tailscale/remote/start.sh
+++ services/tailscale/remote/start.sh
@@ -1,8 +1,8 @@
 #/bin/sh
+echo "PORT=${PORT}";
 echo "TS_USERSPACE=${TS_USERSPACE}";
 echo "TS_ACCEPT_DNS=${TS_ACCEPT_DNS}";
 echo "TS_AUTH_ONCE=${TS_AUTH_ONCE}";
 echo "TAILSCALE_FWD_ADDR=${TAILSCALE_FWD_ADDR}";
-echo "DOMAIN=${DOMAIN}";
 sleep 3;
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
