8d81d3560f6255bef97fdb6446d695038610c37b
Ray Chen <ray@raychen.io>
2024-01-07 18:56:54 +0800

    service(tailscale-remote): More init shenanigans

--- services/tailscale/remote/start.sh
+++ services/tailscale/remote/start.sh
@@ -1,3 +1,4 @@
 #/bin/sh
-rc-service tailscale start
-/usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
+rc-update add tailscale && \
+    rc-service tailscale start && \
+    /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
