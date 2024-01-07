bebc854d4f08cff824f6798a3a9ba90fdff985cf
Ray Chen <ray@raychen.io>
2024-01-07 19:14:17 +0800

    service(tailscale-remote): More ts shenanigans

--- services/tailscale/remote/start.sh
+++ services/tailscale/remote/start.sh
@@ -1,3 +1,7 @@
 #/bin/sh
+export TS_USERSPACE=${TS_USERSPACE}
+export TS_AUTH_ONCE=${TS_AUTH_ONCE}
+export TS_AUTHKEY=${TS_AUTHKEY}
+export TS_ACCEPT_DNS=${TS_ACCEPT_DNS}
 rc-update add tailscale && \
     /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
