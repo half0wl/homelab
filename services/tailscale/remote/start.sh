85f16b129c7d41fd6863360872a296f14b175779
Ray Chen <ray@raychen.io>
2024-01-07 19:24:52 +0800

    service(tailscale-remote): More ts shenanigans

--- services/tailscale/remote/start.sh
+++ services/tailscale/remote/start.sh
@@ -1,12 +1,10 @@
 #/bin/sh
-sleep 3;
-export TS_USERSPACE=${TS_USERSPACE};
-export TS_AUTH_ONCE=${TS_AUTH_ONCE};
-export TS_AUTHKEY=${TS_AUTHKEY};
-export TS_ACCEPT_DNS=${TS_ACCEPT_DNS};
-rc-update add tailscale;
 echo "TS_USERSPACE=${TS_USERSPACE}";
-echo "TS_AUTH_ONCE=${TS_AUTH_ONCE}";
 echo "TS_ACCEPT_DNS=${TS_ACCEPT_DNS}";
+echo "TS_AUTH_ONCE=${TS_AUTH_ONCE}";
+echo "TAILSCALE_FWD_ADDR=${TAILSCALE_FWD_ADDR}";
+echo "DOMAIN=${DOMAIN}";
 sleep 3;
+/usr/sbin/tailscaled --tun=userspace-networking --verbose=1
+sleep 5;
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
